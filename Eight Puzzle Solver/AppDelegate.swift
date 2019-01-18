9278e3391699158e0630a24246f51cced9caaea2
Thomas Lagrange <tplagrange@gmail.com>
2019-01-18 08:20:39 -0500

    Initial Commit

---
+++ Eight Puzzle Solver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Eight Puzzle Solver
+//
+//  Created by Lagrange, Thomas on 1/18/19.
+//  Copyright © 2019 Thomas Lagrange. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Eight Puzzle Solver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Eight Puzzle Solver
+//
+//  Created by Lagrange, Thomas on 1/18/19.
+//  Copyright © 2019 Thomas Lagrange. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

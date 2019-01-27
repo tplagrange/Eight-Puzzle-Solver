83050b5f32d222d34b24bfee639721c48e4052f0
Thomas Lagrange <tplagrange@gmail.com>
2019-01-26 19:54:58 -0500

    UI Additions

--- Eight Puzzle Solver/ViewController.swift
+++ Eight Puzzle Solver/ViewController.swift
@@ -10,21 +10,61 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    @IBOutlet var OutputText: NSTextView!
+    @IBOutlet var AlgorithmMenu: NSPopUpButtonCell!
+    @IBOutlet var DifficultyMenu: NSPopUpButtonCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        solve(puzzle: easy, with: .BreadthFirst)
-
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
+    
+    @IBAction func solvePuzzle(sender: NSButton) {
+        let algorithmItem = AlgorithmMenu.selectedItem!.title
+        let difficultyItem = AlgorithmMenu.selectedItem!.title
+        
+        var algorithm: Algorithm
+        var difficulty: [Int]
+        
+        switch algorithmItem {
+        case "Breadth First":
+            algorithm = .BreadthFirst
+        case "Depth First":
+            algorithm = .DepthFirst
+        case "Uniform Cost":
+            algorithm = .UniformCost
+        case "Best First":
+            algorithm = .BestFirst
+        case "A* One":
+            algorithm = .AStar1
+        case "A* Two":
+            algorithm = .AStar2
+        default:
+            // alert
+            return
+        }
+        
+        switch difficultyItem {
+        case "Easy":
+            difficulty = easy
+        case "Medium":
+            difficulty = medium
+        case "Hard":
+            difficulty = hard
+        default:
+            // alert
+            return
+        }
+        
+        // A-Sync with 5 minute limit
+        solve(puzzle: difficulty, with: algorithm)
+    }
 
 }
-
 
 
